1d54db6015d18f19c3c7b4e32bc6d1bedbf8aaf9
Adam Mescher <admescher@gmail.com>
2019-08-12 18:59:11 -0600

    Add ThemeProvider to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
 import Button from './Components/Button';
+import {ThemeProvider } from 'styled-components';
+const theme = {
+  fontStack: {},
+  colors: {}
+};
 
 function App() {
   return (
-    <main className="App">
-      <section>
-        <h1>HELLO</h1>
-        <Button text={'button label goes here'}/>
-      </section>
-    </main>
+    <ThemeProvider theme={theme}>
+      <main className="App">
+        <section>
+          <h1>HELLO</h1>
+          <Button text={'button label goes here'}/>
+        </section>
+      </main>
+    </ThemeProvider>
   );
 }
 
