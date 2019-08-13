969a9cb9f07a315015293f52a54976c662036c57
Adam Mescher <admescher@gmail.com>
2019-08-12 22:11:28 -0600

    Add Logo component

---
+++ src/Components/Logo/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import StyledLogo from './styled';
+
+const Logo = ({ height, width }) => (
+    <StyledLogo height={height} width={width} />
+)
+
+export default Logo;
---
+++ src/Components/Logo/styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledLogo = styled.div`
+    height: ${props => props.height};
+    width: ${props => props.width};
+`;
+
+export default StyledLogo;
