9be2e89c1840026655e40afd469daa38881e6b56
Adam Mescher <admescher@gmail.com>
2019-08-12 19:13:36 -0600

    Add font stack theme definitions

--- src/App.js
+++ src/App.js
@@ -3,8 +3,48 @@ import './App.css';
 import Button from './Components/Button';
 import {ThemeProvider } from 'styled-components';
 const theme = {
-  fontStack: {},
-  colors: {}
+  fontStack: {
+    sansSerifFontFamily: `'Lecend Deca', sans-serif`,
+    lineHeight: '1.45',
+    bodyFontSize: '16px',
+    h1FontSize: '3.052em',
+    h2FontSize: '2.441em',
+    h3FontSize: '1.953em',
+    h4FontSize: '1.563em',
+    h5FontSize: '1.25em',
+    smallFontSize: '0.8em'
+  },
+  colors: {
+    primary: {
+      one: '',
+      two: '',
+      three: ''
+    },
+    secondary: {
+      one: '',
+      two: '',
+      three: ''
+    },
+    tertiary: {
+      one: '',
+      two: '',
+      three: ''
+    },
+    darkNeutral: {
+      one: '',
+      two: '',
+      three: '',
+      four: '',
+      five: '',
+      six: ''
+    },
+    lightNeutral: {
+      one: '',
+      two: ''
+    }
+
+
+  }
 };
 
 function App() {
