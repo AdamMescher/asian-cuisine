d695875be3a1cd152556790f4ffa17646c0ee880
Adam Mescher <admescher@gmail.com>
2019-09-13 16:41:58 -0600

    Update Button component to properly return the StyledButton compnent

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -4,14 +4,14 @@ import {generateButtonSize} from '../../utils/generateButtonSize';
 import {generateButtonColor} from '../../utils/generateButtonSize';
 import {generateButtonStyle} from '../../utils/generateButtonStyle';
 
-const Button = ({ text, size, color, style }) =>{
+const Button = ({ text, size, color, style }) => (
     <StyledButton
         size={generateButtonSize(size)}
         color={generateButtonColor(color)}
         style={generateButtonStyle(style)}>
             <span>{text}</span>
     </StyledButton>
-}
+)
 
 export default Button;
 
