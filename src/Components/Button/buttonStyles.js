5f39138f0ce54c061ea7a4ffc49c2bbd465de29e
Adam Mescher <admescher@gmail.com>
2019-09-13 16:42:29 -0600

    Create rough outline of what button styles should include - color, size, and style

--- src/Components/Button/buttonStyles.js
+++ src/Components/Button/buttonStyles.js
@@ -1,93 +1,11 @@
-export const defaultButtonStyle = {
-    cursor: '',
-    height: '2rem',
-    fontSize: '1em',
-    fontFamily: '',
-    color: '',
-    bgColor: '',
-    border: '',
-    hoverPadding: '',
-    hoverColor: '',
-    hoverBgColor: '',
-    hoverBorder: '',
-    activePadding: '',
-    activeColor: '',
-    activeBgColor: '',
-    activeBorder: '',
-    disabledPadding: '',
-    disabledColor: '',
-    disabledBgColor: '',
-    disabledBorder: ''
-};
+export const outlineButtonStyle = {};
+export const fillButtonStyle = {};
 
-export const headerButtonStyle = {
-    height: '2rem',
-    fontSize: '',
-    fontFamily: '',
-    color: '',
-    bgColor: '',
-    border: '',
-    hoverColor: '',
-    hoverBgColor: '',
-    hoverBorder: '',
-    activeColor: '',
-    activeBgColor: '',
-    activeBorder: '',
-    disabledColor: '',
-    disabledBgColor: '',
-    disabledBorder: ''
-};
+export const buttonColorWireframe = {};
+export const buttonColorOne = {};
+export const buttonColorTwo = {};
 
-export const heroButtonStyle = {
-    height: '2rem',
-    fontSize: '',
-    fontFamily: '',
-    color: '',
-    bgColor: '',
-    border: '',
-    hoverColor: '',
-    hoverBgColor: '',
-    hoverBorder: '',
-    activeColor: '',
-    activeBgColor: '',
-    activeBorder: '',
-    disabledColor: '',
-    disabledBgColor: '',
-    disabledBorder: ''
-};
-
-export const menuItemButtonStyle = {
-    height: '2rem',
-    fontSize: '',
-    fontFamily: '',
-    color: '',
-    bgColor: '',
-    border: '',
-    hoverColor: '',
-    hoverBgColor: '',
-    hoverBorder: '',
-    activeColor: '',
-    activeBgColor: '',
-    activeBorder: '',
-    disabledColor: '',
-    disabledBgColor: '',
-    disabledBorder: ''
-};
-
-export const textSectionButtonStyle = {
-    height: '2rem',
-    fontSize: '',
-    fontFamily: '',
-    color: '',
-    bgColor: '',
-    border: '',
-    hoverColor: '',
-    hoverBgColor: '',
-    hoverBorder: '',
-    activeColor: '',
-    activeBgColor: '',
-    activeBorder: '',
-    disabledColor: '',
-    disabledBgColor: '',
-    disabledBorder: ''
-};
+export const smallButtonSize = {};
+export const mediumButtonSize = {};
+export const largeButtonSize = {};
+export const extraLargeButtonSize = {};
