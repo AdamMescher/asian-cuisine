1991afa6a762a51f9e7ffd28ff6b20e6044027c3
Adam Mescher <admescher@gmail.com>
2019-09-14 18:26:18 -0600

    Lint Components/Button/index.js

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import StyledButton from './styled';
-import {generateButtonSize} from '../../utils/generateButtonSize';
-import {generateButtonColor} from '../../utils/generateButtonSize';
-import {generateButtonStyle} from '../../utils/generateButtonStyle';
+import { generateButtonSize } from '../../utils/generateButtonSize';
+import { generateButtonColor } from '../../utils/generateButtonSize';
+import { generateButtonStyle } from '../../utils/generateButtonStyle';
 
-const Button = ({ text, size, color, style }) => (
-    <StyledButton
-        size={generateButtonSize(size)}
-        color={generateButtonColor(color)}
-        style={generateButtonStyle(style)}>
+const Button = ({
+    text,
+    size,
+    color,
+    style }) => (
+        <StyledButton
+            size={generateButtonSize(size)}
+            color={generateButtonColor(color)}
+            style={generateButtonStyle(style)}>
             <span>{text}</span>
-    </StyledButton>
-)
+        </StyledButton>
+    );
 
 export default Button;
-
