bd71fcc1f8c51f7efc261f2c739085829806b530
Adam Mescher <admescher@gmail.com>
2019-09-19 19:44:26 -0600

    Import PropTypes correctly

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import StyledButton from './styled';
 import { generateButtonColor, generateButtonSize, generateButtonStyle } from '../../utils/buttonGenerators';
 
