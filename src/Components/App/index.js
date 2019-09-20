9758f234eff17c28991d5b8a9b86432ac2f1992e
Adam Mescher <admescher@gmail.com>
2019-09-19 19:15:16 -0600

    Move theme object from variable in app to standalone theme.js file in ./utils

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,58 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-const theme = {
-  fontStack: {
-    sansSerifFontFamily: `'Lexend Deca', sans-serif'`,
-    lineHeight: '1.45',
-    bodyFontSize: '16px',
-    h1FontSize: '3.052em',
-    h2FontSize: '2.441em',
-    h3FontSize: '1.953em',
-    h4FontSize: '1.563em',
-    h5FontSize: '1.25em',
-    smallFontSize: '0.8em'
-  },
-  colors: {
-    primary: {
-      one: '#58a701',
-      two: '',
-      three: ''
-    },
-    secondary: {
-      one: '#623453',
-      two: '#5270e8',
-    },
-    tertiary: {
-      one: '#ffc511',
-      two: '#ffde7f',
-      three: ''
-    },
-    quaternary: {
-      one: '#fe7749'
-    },
-    quinary: {
-      one: '#02c4a1',
-      two: '#eeeffd',
-      three: '#c7d4dc',
-    },
-    darkNeutral: {
-      one: '',
-      two: '',
-      three: '',
-      four: '',
-      five: '',
-      six: ''
-    },
-    lightNeutral: {
-      one: '#fff3d2',
-      two: '#fde7ad',
-      three: '#ecf9ff',
-      four: '#f7f9fb'
-    }
-  }
-};
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
