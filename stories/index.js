d233f55438f42cc4595bd0c42704c7b54610daae
Adam Mescher <admescher@gmail.com>
2019-09-20 00:05:08 -0600

    Add buttonGenerators and ThemeProvider imports within stories/index.js

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
 import Button from '../src/Components/Button';
+import { theme } from '../src/utils/theme';
+import { generateButtonColor, generateButtonSize, generateButtonStyle } from '../src/utils/buttonGenerators';
+
+
 
 storiesOf('Button', module)
-    .add('Button small outline wireframe', () => <Button text={'hello'} size={'sm'} style={'outline'} color={'wf'}/>)
+    .add('Button small outline wireframe', () => {
+        return (
+            <ThemeProvider theme={theme}>
+                <Button
+                    text={'test button'}
+                    size={generateButtonColor('fill')}
+                    style={generateButtonSize('sm')}
+                    color={generateButtonStyle('wf')} />
+            </ThemeProvider>
+        )
+    });
